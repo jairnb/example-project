5d0f4b9ce45d16d12b232c41122d1fd2576f4590
Pedro Fernandes <jair10.nb@hotmail.com>
2024-06-18 14:05:54 -0400

    merging changes

---
+++ src/test/java/org/pedro/exampleproject/ExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pedro.exampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
